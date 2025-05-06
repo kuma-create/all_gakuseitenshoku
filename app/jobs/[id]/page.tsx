598b2dcc07efb62b29d22a3841480b1b3adbacc0
Yu Kumazaki <yukumazaki@Yus-MacBook-Pro.local>
2025-05-06 16:24:02 +0900

    e

--- app/jobs/[id]/page.tsx
+++ app/jobs/[id]/page.tsx
@@ -125,7 +125,7 @@ export default function JobDetailPage({ params }: { params: { id: string } }) {
             location,
             salary_min,
             salary_max,
-            company:company_id (
+            company:companies (
               name,
               logo_url
             )
