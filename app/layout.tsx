fe0a16c96530a6eaea652792f668d7cb863bc712
Yu Kumazaki <yukumazaki@Yus-MacBook-Pro.local>
2025-07-08 21:25:41 +0900

    r

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,11 +86,35 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             gtag('config', 'G-FNPBR7XJT6', { send_page_view: false });
           `}
         </Script>
+        {/* Meta Pixel (Facebook) */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '9933035836808765');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body
         className={`${inter.className} overflow-x-hidden bg-background text-foreground`}
         suppressHydrationWarning
       >
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=9933035836808765&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <Suspense fallback={null}>
           <Providers>
             {/* クライアント側で認可判定 */}
