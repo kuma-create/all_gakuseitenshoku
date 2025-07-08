2910811ca4a42d18cdca8c244ba46076a9faaac1
Yu Kumazaki <yukumazaki@Yus-MacBook-Pro.local>
2025-07-08 15:14:48 +0900

    r

--- app/lp/students/fee/page.tsx
+++ app/lp/students/fee/page.tsx
@@ -27,6 +27,7 @@ import Image from "next/image"
 import heroImg from "@/public/shukatu.jpg";
 
 import { useRouter } from "next/navigation"
+import Script from "next/script";
 
 // Lazy‑load salary estimator only when needed
 const loadEstimator = () =>
@@ -206,6 +207,30 @@ export default function LandingPage() {
 
   return (
     <div className="min-h-screen bg-white">
+      {/* Meta Pixel Code */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1367684414304232');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1367684414304232&ev=PageView&noscript=1"
+        />
+      </noscript>
+      {/* End Meta Pixel Code */}
 
       {/* Hero Section */}
       <section className="pt-12 pb-10 md:pb-16">
