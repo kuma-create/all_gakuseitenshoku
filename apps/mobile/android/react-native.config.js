ac814939d1b17f10e18e9075549e3844107e248c
千葉 <h.chiba@makeculture.jp>
2025-08-25 20:53:14 +0900

    Gradle通った

---
+++ apps/mobile/android/react-native.config.js
@@ -0,0 +1,9 @@
+// Local config so RNGP finds packageName when executed from /android
+module.exports = {
+  project: {
+    android: {
+      sourceDir: './android',
+      packageName: 'co.gakuten.app',
+    },
+  },
+};
