4bd445ba1c59754044af0e14f5d2f1c558ad7fc6
千葉 <h.chiba@makeculture.jp>
2025-08-25 21:15:33 +0900

    #

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -15,3 +15,8 @@ module.exports = function (api) {
     ],
   };
 };
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['react-native-reanimated/plugin'],
+};
